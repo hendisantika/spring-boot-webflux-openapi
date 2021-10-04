55efe350bd6dc3dec1e5580beb75a7b014b19f91
Hendi Santika <hendisantika@gmail.com>
2021-10-04 08:22:06 +0700

    #20 - Add routerFunction in RouterConfig

--- src/main/java/com/hendisantika/config/RouterConfig.java
+++ src/main/java/com/hendisantika/config/RouterConfig.java
@@ -1,9 +1,22 @@
 package com.hendisantika.config;
 
+import com.hendisantika.dto.Customer;
 import com.hendisantika.handler.CustomerHandler;
 import com.hendisantika.handler.CustomerStreamHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -26,6 +39,89 @@ public class RouterConfig {
     @Autowired
     private CustomerStreamHandler streamHandler;
 
+    @Bean
+    @RouterOperations(
+            {
+                    @RouterOperation(
+                            path = "/router/customers",
+                            produces = {
+                                    MediaType.APPLICATION_JSON_VALUE
+                            },
+                            method = RequestMethod.GET,
+                            beanClass = CustomerHandler.class,
+                            beanMethod = "loadCustomers",
+                            operation = @Operation(
+                                    operationId = "loadCustomers",
+                                    responses = {
+                                            @ApiResponse(
+                                                    responseCode = "200",
+                                                    description = "successful operation",
+                                                    content = @Content(schema = @Schema(
+                                                            implementation = Customer.class
+                                                    ))
+                                            )
+                                    }
+                            )
+                    ),
+                    @RouterOperation(
+                            path = "/router/customer/{input}",
+                            produces = {
+                                    MediaType.APPLICATION_JSON_VALUE
+                            },
+                            method = RequestMethod.GET,
+                            beanClass = CustomerHandler.class,
+                            beanMethod = "findCustomer",
+                            operation = @Operation(
+                                    operationId = "findCustomer",
+                                    responses = {
+                                            @ApiResponse(
+                                                    responseCode = "200",
+                                                    description = "successful operation",
+                                                    content = @Content(schema = @Schema(
+                                                            implementation = Customer.class
+                                                    ))
+                                            ),
+                                            @ApiResponse(responseCode = "404", description = "customer not found with" +
+                                                    " given id")
+                                    },
+                                    parameters = {
+                                            @Parameter(in = ParameterIn.PATH, name = "input")
+                                    }
+
+                            )
+
+                    ),
+                    @RouterOperation(
+                            path = "/router/customer/save",
+                            produces = {
+                                    MediaType.APPLICATION_JSON_VALUE
+                            },
+                            method = RequestMethod.POST,
+                            beanClass = CustomerHandler.class,
+                            beanMethod = "saveCustomer",
+                            operation = @Operation(
+                                    operationId = "saveCustomer",
+                                    responses = {
+                                            @ApiResponse(
+                                                    responseCode = "200",
+                                                    description = "successful operation",
+                                                    content = @Content(schema = @Schema(
+                                                            implementation = String.class
+                                                    ))
+                                            )
+                                    },
+                                    requestBody = @RequestBody(
+                                            content = @Content(schema = @Schema(
+                                                    implementation = Customer.class
+                                            ))
+                                    )
+
+                            )
+
+
+                    )
+            }
+    )
     public RouterFunction<ServerResponse> routerFunction() {
         return RouterFunctions.route()
                 .GET("/router/customers", handler::loadCustomers)
