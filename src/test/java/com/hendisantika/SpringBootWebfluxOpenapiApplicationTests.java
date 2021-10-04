13e02dd16394f977aebacf4127de76e574bc3ae0
Hendi Santika <hendisantika@gmail.com>
2021-10-04 08:26:05 +0700

    #25 - Add testMono Test Case

--- src/test/java/com/hendisantika/SpringBootWebfluxOpenapiApplicationTests.java
+++ src/test/java/com/hendisantika/SpringBootWebfluxOpenapiApplicationTests.java
@@ -2,12 +2,17 @@ package com.hendisantika;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class SpringBootWebfluxOpenapiApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testMono() {
+        Mono<?> monoString = Mono.just("Edannnnn")
+                .then(Mono.error(new RuntimeException("Exception occured")))
+                .log();
+        monoString.subscribe(System.out::println, (e) -> System.out.println(e.getMessage()));
     }
 
 }
