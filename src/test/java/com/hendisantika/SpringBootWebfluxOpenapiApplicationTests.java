c9a30c11c44f7fe198ebc4d1bafa4ca0088188f2
Hendi Santika <hendisantika@gmail.com>
2021-10-04 08:10:31 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/SpringBootWebfluxOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxOpenapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
