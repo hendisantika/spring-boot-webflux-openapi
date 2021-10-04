fb1c06c1dd2b9bfd8163114eca1c93c09c3c0e14
Hendi Santika <hendisantika@gmail.com>
2021-10-04 08:18:26 +0700

    #14 - Add loadCustomers in CustomerHandler

--- src/main/java/com/hendisantika/handler/CustomerHandler.java
+++ src/main/java/com/hendisantika/handler/CustomerHandler.java
@@ -1,8 +1,13 @@
 package com.hendisantika.handler;
 
+import com.hendisantika.dto.Customer;
 import com.hendisantika.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +23,10 @@ public class CustomerHandler {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    public Mono<ServerResponse> loadCustomers(ServerRequest request) {
+        Flux<Customer> customerList = customerRepository.getCustomerList();
+        return ServerResponse.ok().body(customerList, Customer.class);
+    }
+
 }
