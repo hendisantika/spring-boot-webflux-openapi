20b06716da15b5ef4d288d0a736c6486d3d71bbc
Hendi Santika <hendisantika@gmail.com>
2021-10-04 08:24:25 +0700

    #23 - Add getAllCustomers in CustomerController

--- src/main/java/com/hendisantika/controller/CustomerController.java
+++ src/main/java/com/hendisantika/controller/CustomerController.java
@@ -1,9 +1,13 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.Customer;
 import com.hendisantika.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerService.loadAllCustomers();
+    }
 }
