61516e96406e0e543451779ee4a1d10e0cf86bfa
Hendi Santika <hendisantika@gmail.com>
2021-10-04 08:22:58 +0700

    #21 - Add OpenAPIDefinition in Main Class

--- src/main/java/com/hendisantika/SpringBootWebfluxOpenapiApplication.java
+++ src/main/java/com/hendisantika/SpringBootWebfluxOpenapiApplication.java
@@ -1,9 +1,16 @@
 package com.hendisantika;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Spring WebFlux CRUD Example",
+        version = "1.0",
+        description = "Spring WebFlux CRUD Example Sample documents"
+))
 public class SpringBootWebfluxOpenapiApplication {
 
     public static void main(String[] args) {
