882a83c3f3e76978a292b0c03af7d01d20532e9d
Hendi Santika <hendisantika@gmail.com>
2021-10-04 08:23:44 +0700

    #22 - Add CustomerController

---
+++ src/main/java/com/hendisantika/controller/CustomerController.java
@@ -0,0 +1,22 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-openapi
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/10/21
+ * Time: 08.23
+ */
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+}
