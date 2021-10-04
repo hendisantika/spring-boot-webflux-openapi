c20d57038280463fe0b7bafacd728af9c3cdf9ef
Hendi Santika <hendisantika@gmail.com>
2021-10-04 08:14:28 +0700

    #6 - Add sleepExecution in CustomerRepository

--- src/main/java/com/hendisantika/repository/CustomerRepository.java
+++ src/main/java/com/hendisantika/repository/CustomerRepository.java
@@ -13,5 +13,11 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class CustomerRepository {
-
+    private static void sleepExecution(int i) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
