599f6d6108a1c7fa0fe69e7b62cbf038936e4b57
Hendi Santika <hendisantika@gmail.com>
2021-10-04 08:14:07 +0700

    #5 - Add CustomerRepository

---
+++ src/main/java/com/hendisantika/repository/CustomerRepository.java
@@ -0,0 +1,17 @@
+package com.hendisantika.repository;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-openapi
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/10/21
+ * Time: 08.13
+ */
+@Component
+public class CustomerRepository {
+
+}
